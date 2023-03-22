42cb0487dfdbe99949b6d552a29e18af13426a46
DmytroAV <87889953+DmytroAV@users.noreply.github.com>
2023-03-22 05:19:13 +0000

    Deploying to gh-pages from @ DmytroAV/js-hw-09@cfa3882075bce84da651e960e90fed224fc600b6 🚀

---
+++ 01-color-switcher.5212ca6c.js
@@ -0,0 +1,2 @@
+!function(){var o,n=7,c=9;n=(o=[c=(n+=c)-c,n-=c])[0],c=o[1],console.log("a, b :>> ",n,c);var a;[0,1,2,3,4,5,6,7,8,10].forEach((function(o,n){o!==n&&(a=n)})),console.log(a)}();
+//# sourceMappingURL=01-color-switcher.5212ca6c.js.map
