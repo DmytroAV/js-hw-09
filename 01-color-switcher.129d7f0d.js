ff6d34a5df273e09f60c279157046888ab0b5d19
DmytroAV <87889953+DmytroAV@users.noreply.github.com>
2023-04-18 06:26:59 +0000

    Deploying to gh-pages from @ DmytroAV/js-hw-09@7e44ec5464cb36ace118ef13c3109df7f2d60dca 🚀

--- 01-color-switcher.129d7f0d.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,n=setInterval((function(){return document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,clearInterval(n)}));var n=null}();
-//# sourceMappingURL=01-color-switcher.129d7f0d.js.map
